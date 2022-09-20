afa82a7f7c536f490a02b85baed79b0e4a0723df
Литвинов Владислав Олегович <Litvinov_vo@tochka.com>
2022-09-20 20:57:05 +0500

    шаблон проекта

---
+++ src/test/java/com/demoqa/AutomationPracticeFormTest.java
@@ -0,0 +1,11 @@
+package com.demoqa;
+
+/**
+ * @author Vlad Litvinov
+ *
+ *         класс для тестов <a href="https://demoqa.com/automation-practice-form">формы
+ *         регистрации</a>
+ */
+public class AutomationPracticeFormTest {
+
+}
