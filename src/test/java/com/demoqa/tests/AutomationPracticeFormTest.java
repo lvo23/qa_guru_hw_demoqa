9b15535b09ffefcb2b339c6a5f527301e1a7c2e1
litvinov <litvinov.lvo@gmail.com>
2022-09-24 22:48:30 +0500

    переписал тест с использованием PageObject

--- src/test/java/com/demoqa/tests/AutomationPracticeFormTest.java
+++ src/test/java/com/demoqa/tests/AutomationPracticeFormTest.java
@@ -1,19 +1,19 @@
 package com.demoqa.tests;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.executeJavaScript;
-import static com.codeborne.selenide.Selenide.open;
-import static com.demoqa.utils.Highlighter.highlight;
+import static com.demoqa.utils.DataGenerator.generateAddress;
+import static com.demoqa.utils.DataGenerator.generateEmail;
+import static com.demoqa.utils.DataGenerator.generateFirstName;
+import static com.demoqa.utils.DataGenerator.generateGender;
+import static com.demoqa.utils.DataGenerator.generateHobby;
+import static com.demoqa.utils.DataGenerator.generateLastName;
+import static com.demoqa.utils.DataGenerator.generatePhoneNumber;
+import static com.demoqa.utils.DataGenerator.generateSubject;
 
-import java.util.Random;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.demoqa.BaseTest;
-import com.github.javafaker.Faker;
+import com.demoqa.pages.RegistrationFormPage;
 
 /**
  * @author Vlad Litvinov
@@ -24,87 +24,53 @@ import com.github.javafaker.Faker;
 public class AutomationPracticeFormTest extends BaseTest {
 
     /**
-     * инициализация java-faker
+     * Инициализация страницы с формой регистрации
      */
-    Faker faker = new Faker();
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
 
     /**
      * тест на заполнение формы регистрации
      */
     @Test
+    @DisplayName("Заполнение всех полей регистрационной формы")
     void fillRegistrationFormTest() {
 
-        String registrationFormUrl = "/automation-practice-form";
-
-        // генерация и хардкод тестовых данных
-        String firstName = faker.name().firstName();
-        String lastName = faker.name().lastName();
-        String email = faker.internet().emailAddress();
-
-        // гендер выбираем рандомно, чтобы максимально убрать хардкод
-        String[] genders = { "Male", "Female", "Other" };
-        String gender = genders[new Random().nextInt(genders.length)];
-
-        String phoneNumber = faker.phoneNumber().subscriberNumber(10);
+        // данные для тестов, которые генерируются
+        String firstName = generateFirstName();
+        String lastName = generateLastName();
+        String email = generateEmail();
+        String gender = generateGender();
+        String phoneNumber = generatePhoneNumber();
+        String hobby = generateHobby();
+        String subject = generateSubject();
+        String address = generateAddress();
 
         // про то, как моя хитрость не взлетела – читать в README
+        // оставил хардкодом то, что без генерации в самом тесте
         String date = "20";
         String month = "September";
         String year = "2022";
         String birthDay = date + " " + month + "," + year;
-
-        String[] subjects = { "English", "Math", "Computer Science" };
-        String subject = subjects[new Random().nextInt(subjects.length)];
-
-        String[] hobbies = { "Sports", "Reading", "Music" };
-        String hobby = hobbies[new Random().nextInt(hobbies.length)];
-
         String imagePath = "image_poo.jpg";
-        String address = faker.address().streetAddress();
-
-        // вот тут чёт так и не придумал как сделать подбор
         String state = "NCR";
         String city = "Delhi";
 
         // сам тест
-        open(registrationFormUrl);
-
-        highlight(executeJavaScript("$('footer').remove()"));
-        highlight(executeJavaScript("$('#fixedban').remove()"));
-
-        highlight($("#firstName").setValue(firstName));
-        highlight($("#lastName").setValue(lastName));
-
-        highlight($("#userEmail").setValue(email));
-
-        $$(".custom-radio").findBy(text(gender)).click();
-
-        highlight($("#userNumber").setValue(phoneNumber));
-
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption(month);
-        $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker__day--020").click();
-
-        highlight($("#subjectsInput").setValue(subject).pressEnter());
-
-        $$(".custom-control-label").findBy(text(hobby)).click();
-        $("#uploadPicture").uploadFromClasspath(imagePath);
-
-        highlight($("#currentAddress").setValue(address));
-
-        $("#state").click();
-        $(byText(state)).click();
-        $("#city").click();
-        $(byText(city)).click();
-
-        $("#submit").click();
+        registrationFormPage.openPage().setFirstName(firstName).setLastName(lastName)
+                .setEmail(email).setGender(gender).setNumber(phoneNumber)
+                .setBirthDate(date, month, year).setHobbies(hobby).setSubjects(subject)
+                .setUploadPicture(imagePath).setAddress(address).setState("NCR").setCity("Delhi")
+                .setSubmit();
 
         // проверяем успех заполнения формы
-        highlight($(".modal-header").shouldHave(text("Thanks for submitting the form")));
-        highlight($(".table-responsive").shouldHave(text(firstName + " " + lastName), text(email),
-                text(gender), text(phoneNumber), text(birthDay), text(subject), text(hobby),
-                text(imagePath), text(address), text(state + " " + city)));
+        // todo наверное название (key) можно вынести в енам какой-нибудь
+        registrationFormPage.checkResultsTableVisible().checkTableResult("Student Name", firstName)
+                .checkTableResult("Student Email", lastName).checkTableResult("Gender", gender)
+                .checkTableResult("Mobile", phoneNumber)
+                .checkTableResult("Date of Birth", birthDay).checkTableResult("Subjects", subject)
+                .checkTableResult("Hobbies", hobby).checkTableResult("Picture", imagePath)
+                .checkTableResult("Address", address)
+                .checkTableResult("State and City", state + " " + city);
     }
 
 }
