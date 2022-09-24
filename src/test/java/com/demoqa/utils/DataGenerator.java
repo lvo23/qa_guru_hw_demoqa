038f40e5ff446addbd47c889e1934cdcce14afab
litvinov <litvinov.lvo@gmail.com>
2022-09-24 21:46:57 +0500

    добавил жавадоки

--- src/test/java/com/demoqa/utils/DataGenerator.java
+++ src/test/java/com/demoqa/utils/DataGenerator.java
@@ -24,27 +24,47 @@ public class DataGenerator {
         return faker.name().firstName();
     }
 
+    /**
+     * генерация фамилии
+     * @return строку с рандомной фамилией
+     */
     public static String generateLastName() {
 
         return faker.name().lastName();
     }
 
+    /**
+     * генерация емейла
+     * @return строку с рандомным емейлом
+     */
     public static String generateEmail() {
 
         return faker.internet().emailAddress();
     }
 
+    /**
+     * генерация гендера (пола)
+     * @return строку с рандомным гендером
+     */
     public static String generateGender() {
 
         String[] genders = { "Male", "Female", "Other" };
         return genders[new Random().nextInt(genders.length)];
     }
 
+    /**
+     * генерация номера телефона 10-значного
+     * @return строку с рандомным 10-значного номера телефона
+     */
     public static String generatePhoneNumber() {
 
         return faker.phoneNumber().subscriberNumber(10);
     }
 
+    /**
+     * генерация предмета изучения (математика, анлийский итд)
+     * @return строку с рандомным предметом
+     */
     public static String generateSubject() {
 
         String[] subjects = { "English", "Math", "Computer Science" };
@@ -52,12 +72,20 @@ public class DataGenerator {
 
     }
 
+    /**
+     * генерация хобби
+     * @return строку с рандомным хобби
+     */
     public static String generateHobby() {
         String[] hobbies = { "Sports", "Reading", "Music" };
         return hobbies[new Random().nextInt(hobbies.length)];
 
     }
 
+    /**
+     * генерация адреса
+     * @return строку с рандомным адресом
+     */
     public static String generateAddress() {
         return faker.address().streetAddress();
     }
