3c4d71406d27c4fe8f9d061ac83c20af816c5ad7
litvinov <litvinov.lvo@gmail.com>
2022-09-24 22:17:08 +0500

    заготовка под PageObject

--- src/test/java/com/demoqa/pages/components/CalendarComponent.java
+++ src/test/java/com/demoqa/pages/components/CalendarComponent.java
@@ -1,8 +1,25 @@
 package com.demoqa.pages.components;
 
+import static com.codeborne.selenide.Selenide.$;
+
 /**
  * @author Vlad Litvinov компонента элемента Календарь
  */
 public class CalendarComponent {
 
+    /**
+     * установить дату в компоненте Календарь
+     * @param day заданный день
+     * @param month заданный месяц
+     * @param year заданный год
+     * @return CalendarComponent
+     */
+    public CalendarComponent setDate(String day, String month, String year) {
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
+
+        return this;
+    }
 }
--- src/test/java/com/demoqa/pages/components/ResultsModal.java
+++ src/test/java/com/demoqa/pages/components/ResultsModal.java
@@ -1,8 +1,40 @@
 package com.demoqa.pages.components;
 
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
 /**
  * @author Vlad Litvinov компонена модального окна с результатами
  */
 public class ResultsModal {
 
+    /**
+     * текст заголовка в таблице результатов
+     */
+    private final static String TITLE_TEXT = "Thanks for submitting the form";
+
+    /**
+     * проверка, что модалка видна
+     * @return ResultsModal
+     */
+    public ResultsModal checkVisible() {
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text(TITLE_TEXT));
+
+        return this;
+    }
+
+    /**
+     * проверк значений в модалке
+     * @param key наименование значения
+     * @param value само значение
+     * @return ResultsModal
+     */
+    public ResultsModal checkResult(String key, String value) {
+        $(".table-responsive table").$(byText(key))
+                .parent().shouldHave(text(value));
+        return this;
+    }
 }
