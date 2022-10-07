2e80f8f0793fc9bcf7ff9177479449ed9277a9e2
litvinov <litvinov.lvo@gmail.com>
2022-10-07 19:09:59 +0500

    расширил базовый класс тестов с методами аттачей

--- src/test/java/com/lvo23/BaseTest.java
+++ src/test/java/com/lvo23/BaseTest.java
@@ -1,22 +1,46 @@
 package com.lvo23;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.lvo23.helpers.AllureAttachments;
+
+import io.qameta.allure.selenide.AllureSelenide;
 
 /**
  * @author Vlad Litvinov
  */
 public class BaseTest {
+
     /**
      * метод сет-ап, который выполнится перед всеми тестами
      */
     @BeforeAll
     static void setUp() {
 
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-//        Configuration.holdBrowserOpen = true;
+        // Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments() {
+
+        AllureAttachments.screenshotAs("Последний скриншот");
+        AllureAttachments.pageSource();
+        AllureAttachments.browserConsoleLogs();
+        AllureAttachments.addVideo();
     }
 
 }
